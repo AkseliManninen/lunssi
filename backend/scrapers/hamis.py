7cbfd00a0bf081096081f9c039ad5648431882dc
Tony Lam <tony.lam@iki.fi>
2026-04-24 22:40:49 +0300

    update: hämis price increase

--- backend/scrapers/hamis.py
+++ backend/scrapers/hamis.py
@@ -8,7 +8,7 @@ class HamisScraper(RestaurantScraper):
             "Hämäläis-Osakunta",
             "https://hys.net/osakuntabaari/ruokalista/",
             "https://maps.app.goo.gl/dUYzeugzayHzYicN7",
-            "2,95€",
+            "3,10€",
             "11:00 - 15:00",
         )
         self.is_student_cantine = True
