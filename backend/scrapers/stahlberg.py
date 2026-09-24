8a74e40c13dad55f0d16f39fd37cc731530516da
Yann Jorelle <yann@jorelle.fi>
2026-09-24 09:33:19 +0300

    fix: ståhlberg scraper

--- backend/scrapers/stahlberg.py
+++ backend/scrapers/stahlberg.py
@@ -8,20 +8,22 @@ class StahlbergScraper(RestaurantScraper):
             "Ståhlberg Tampella",
             "https://stahlbergkahvilat.fi/lounasravintolat/tampella/",
             "https://maps.app.goo.gl/G4tBvRLy1q3VZCns5",
-            "13,50€ (Buffet)",
+            "13,90€ (Buffet)",
             "10:30 - 15:00",
         )
         self.region = "tampere"
 
     def parse_menu(self, soup, lang):
-        day_id = self.get_day_name().lower()
-        day_div = soup.find("div", {"id": day_id})
-        if not day_div:
-            return self.fallback_menu[lang]
+        day_name = self.get_day_name().lower()
+        day_heading = soup.find(
+            "h3", string=lambda text: text and text.strip().lower().startswith(day_name)
+        )
+        if not day_heading:
+            return self.menu_fallback[lang]
 
-        menu_table = day_div.find("table", {"class": "tablepress"})
+        menu_table = day_heading.find_next("table", class_="ruokalista")
         if not menu_table:
-            return self.fallback_menu[lang]
+            return self.menu_fallback[lang]
 
         menu_items = []
         for row in menu_table.find_all("tr"):
@@ -33,4 +35,4 @@ class StahlbergScraper(RestaurantScraper):
 
         menu_items = list(dict.fromkeys(filter(None, menu_items)))
 
-        return menu_items if menu_items else self.fallback_menu[lang]
+        return menu_items if menu_items else self.menu_fallback[lang]
